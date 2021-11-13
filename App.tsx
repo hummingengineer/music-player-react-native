c6d170be278ae11855582f08082abd726bf7801a
hummingengineer <hummingengineer@gmail.com>
2021-11-13 17:59:42 +0900

    feat: Add react-native-track-player settings

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import React, {useEffect} from 'react';
+import TrackPlayer, {Capability} from 'react-native-track-player';
 
 import {requestPermissions} from './src/utils';
 
@@ -6,6 +7,21 @@ export default function App() {
   useEffect(() => {
     const setup = async () => {
       await requestPermissions();
+
+      // if app was relaunched and music was already playing, we don't setup again.
+      if ((await TrackPlayer.getCurrentTrack()) !== null) return;
+
+      await TrackPlayer.setupPlayer({});
+      await TrackPlayer.updateOptions({
+        stopWithApp: true,
+        capabilities: [
+          Capability.Play,
+          Capability.Pause,
+          Capability.Stop,
+          Capability.SkipToNext,
+          Capability.SkipToPrevious,
+        ],
+      });
     };
 
     setup();
---
+++ src/utils/services/TrackPlayerService.ts
@@ -0,0 +1,25 @@
+import TrackPlayer, {Event} from 'react-native-track-player';
+
+module.exports = async function () {
+  TrackPlayer.addEventListener(Event.RemotePlay, () => TrackPlayer.play());
+  TrackPlayer.addEventListener(Event.RemotePause, () => TrackPlayer.pause());
+  TrackPlayer.addEventListener(Event.RemoteStop, () => TrackPlayer.destroy());
+  TrackPlayer.addEventListener(Event.RemoteNext, async () => {
+    try {
+      await TrackPlayer.skipToNext();
+    } catch {
+      // If there is no next track, replay the current track.
+      await TrackPlayer.seekTo(0);
+    }
+  });
+  TrackPlayer.addEventListener(Event.RemotePrevious, async () => {
+    try {
+      await TrackPlayer.skipToPrevious();
+    } catch {
+      // If there is no previous track, replay the current track.
+      await TrackPlayer.seekTo(0);
+    }
+  });
+};
+
+export {};
