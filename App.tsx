2d83cedc01a24599779c20d554056d72788f56f8
hummingengineer <hummingengineer@gmail.com>
2021-11-23 12:55:29 +0900

    feat: Add App.tsx

--- App.tsx
+++ App.tsx
@@ -1,7 +1,15 @@
 import React, {useEffect} from 'react';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import TrackPlayer, {Capability} from 'react-native-track-player';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
+import type {RootStackParamList} from './src/types';
+import {LibraryScreen, PlayerScreen} from './src/screens';
 import {requestPermissions} from './src/utils';
+
+const RootStack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   useEffect(() => {
@@ -22,10 +30,37 @@ export default function App() {
           Capability.SkipToPrevious,
         ],
       });
+
+      // if app does not have like and playlist items, initialize them.
+      if (!(await AsyncStorage.getItem('likes'))) {
+        await AsyncStorage.setItem('likes', JSON.stringify([]));
+      }
+      if (!(await AsyncStorage.getItem('playlist'))) {
+        await AsyncStorage.setItem('playlist', JSON.stringify([]));
+      }
     };
 
     setup();
   }, []);
 
-  return <></>;
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <RootStack.Navigator initialRouteName="Library">
+          <RootStack.Group>
+            <RootStack.Screen
+              name="Library"
+              component={LibraryScreen}
+              options={{title: 'Library', headerTitleAlign: 'center'}}
+            />
+            <RootStack.Screen
+              name="Player"
+              component={PlayerScreen}
+              options={{headerShown: false}}
+            />
+          </RootStack.Group>
+        </RootStack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
 }
